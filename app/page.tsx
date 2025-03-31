62d2cc63920df35507998c82671a47afab90a213
aakashsaini09 <aakashsaini948585@gmail.com>
2025-03-31 18:22:38 +0530

    home grid

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex">
       <Sidebar/>
-      {/* <Homepage/> */}
+      <div className="text-black grid grid-cols-1 md:grid-cols-2">
+         {/* Business List */}
+         <div>Business List</div>
+         {/* Gpogle Map */}
+         <div> Gpogle Map</div>
+      </div>
     </div>
   );
 }
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -9,22 +9,25 @@ const Sidebar = () => {
     const data = [
         {
             id: "1",
-            img: './',
-            name: 'Fav'
+            img: './search.png',
+            name: 'Search'
         },
         {
             id: "2",
-            img: 'dkf',
-            name: 'Search'
+            img: './heart.png',
+            name: 'Fav'
         },
     ]
   return (
     <>
-      <div className='w-[100px] h-screen bg-gray-600 flex flex-col p-2 items-center space-y-4 sticky top-0'>
-        <img src="./logo.png" width={50} height={50} alt="not found" />
+      <div className='w-[100px] h-screen bg-gray-50 flex flex-col p-2 items-center space-y-4 sticky top-0 z-10 shadow-2xl shadow-purple-400'>
+        <img src="./logo.png" width={50} height={50} alt="not found" className='gap-4 flex items-center justify-center py-4' />
             {data.map((item:dataType) => {
                 return (
-                    <img src={item.img} alt="" />
+                    <div id={item.id} className='hover:bg-gray-500 rounded-sm p-2'>
+                        <img src={item.img} alt="" className='w-8 hover:text-purple-700 mx-auto gap-2' />
+                        {/* {item.name} */}
+                    </div>
                 )
             })}
       </div>
