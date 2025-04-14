fb955e2767519cecaf669ac6550d7ece9a513b57
aakashsaini09 <aakashsaini948585@gmail.com>
2025-04-14 16:02:26 +0530

    marker done

--- components/Marker.tsx
+++ components/Marker.tsx
@@ -1,5 +1,5 @@
 import { BusinessListContext } from '@/context/BusinessListContext'
-import { MarkerF } from '@react-google-maps/api'
+import { InfoBox, MarkerF } from '@react-google-maps/api'
 import React, { useContext } from 'react'
 
 const Marker = ({userLocation}: {userLocation: any}) => {
@@ -13,13 +13,37 @@ const Marker = ({userLocation}: {userLocation: any}) => {
   
   return (
     <div>
-      { businessList && businessList.map((buss: any)=> (
+      { businessList && businessList.map((buss: any, index)=>index<=5 && (
         <MarkerF 
           position={buss.geometry.location}
-          ></MarkerF>
+          icon={{
+            url:'/userLocation.png',
+            scaledSize: new google.maps.Size(50, 50)
+          }}>
+             <InfoBox position={buss.geometry.location}>
+                  <div
+                    style={{
+                      backgroundColor: "#c084fc",
+                      opacity: 1,
+                      padding: 7,
+                      color: "white",
+                      borderRadius: 10,
+                      width: 100,
+                    }}
+                  >
+                    <div style={{ fontSize: 13, color: '#08233B'}}>
+                      {buss.name}
+                    </div>
+                  </div>
+                </InfoBox>
+          </MarkerF>
       ))}
       <MarkerF
         position={userLocation}
+        icon={{
+          url:'/locatio.png',
+          scaledSize: new google.maps.Size(50, 50)
+        }}
       ></MarkerF>
     </div>
   )
