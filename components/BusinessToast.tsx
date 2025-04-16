692f6f092fc29921dca8497ec1657d64158653c6
aakashsaini09 <aakashsaini948585@gmail.com>
2025-04-16 16:32:57 +0530

    calc distance

--- components/BusinessToast.tsx
+++ components/BusinessToast.tsx
@@ -1,13 +1,35 @@
 import { SelectedBusinessContext } from '@/context/SelectedBusinessContext'
 import Image from 'next/image'
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 
 const BusinessToast = ({userLocation}: {userLocation: any}) => {
+    const [distance, setDistance] = useState<string | undefined>()
     const {selectedBusiness, setSelectedBusiness} = useContext(SelectedBusinessContext)
     useEffect(() => {
       
     }, [selectedBusiness])
+    const calculateDistance = ({lat1, lon1, lat2, lon2}: {lat1: number, lon1: number, lat2: number, lon2: number}) => {
+     
+        const earthRadius = 6371; // in kilometers
     
+        const degToRad = (deg: number) => {
+          return deg * (Math.PI / 180);
+        };
+    
+        const dLat = degToRad(lat2 - lat1);
+        const dLon = degToRad(lon2 - lon1);
+    
+        const a =
+          Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+          Math.cos(degToRad(lat1)) * Math.cos(degToRad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    
+        const distance = earthRadius * c;
+       
+        setDistance(distance.toFixed(1))
+        return distance.toFixed(2); // Return the distance with 2 decimal places
+      };
   return (
     <>{selectedBusiness?.name ? 
     <div className='fixed bottom-5 right-5 z-30 flex items-center bg-purple-400 p-5 rounded-2xl gap-5'>
